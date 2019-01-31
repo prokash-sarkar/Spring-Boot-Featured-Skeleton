3f3c5c0fbc313861a1cdcb75cc91978eeef012c4
anik <anik.hasan@blackswan.com>
2019-01-31 21:21:47 +0600

    initialising project

---
+++ src/main/kotlin/com/firti/web/firti/FirtiApplication.kt
@@ -0,0 +1,12 @@
+package com.firti.web.firti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirtiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirtiApplication>(*args)
+}
+
